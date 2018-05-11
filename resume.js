6ac1377189a2f01e00b35d098193078ed708df89
fowler013 <fowler0613@gmail.com>
2018-05-11 11:44:40 -0500

    modified commit

--- resume.js
+++ resume.js
@@ -2,31 +2,38 @@ var name= "Tevin Fowler"
 var career="Web Developer/ Student"
 var bio="I am a web-developer student at Innovation Depot. I am from Birmingham Alabama and grewup in Homewood, Alabama."
 var interests=["soccer", "music", "video games", "reading", "coding"]
-var past=[job="homewood city schools, assistant teacher"]
+var past=[job="homewood city schools, assistant teacher", "B&N: I orked at the front desk and also assisted students with looking and picking certain text books", "Homewood Soccer Club; was a youth soccer coach for a few years"]
 var mySkills=["HTML", "CSS", "Javascript"]
+var BAM=true
 
-console.log("name" + ":" + " " + name);
-console.log("career" + ":" + " " + career);
-console.log("My Bio" + ":" + " " + bio);
+console.log("NAME" + ":" + " " + name);
+console.log("CAREER" + ":" + " " + career);
+console.log("BIO" + ":" + " " + bio);
 
 
 function myInterests(){
+    console.log("MY INTEREST:");
     for(i=0; i<interests.length; i++){
-        console.log("My Interest" + ":" + " " + interests[i]);
+        console.log("*" + interests[i]);
     };
 }myInterests(interests);
 
 function myPast(){
+    console.log("MY PREVIOUS EXPERIENCE:");
     for (let i = 0; i < past.length; i++){
-       console.log("My Previous Experience" + ":" + " " +  past[i]);
+       console.log("*" + past[i]);
         
-    }
+    };
 }myPast()
 
 
 function Skills(){
+    console.log("MY SKILLS:");
     for ( let i=0; i < mySkills.length; i++){
-        console.log("My Skills" + ":" + " " + mySkills[i]);
+        console.log("*" + mySkills[i]);
     }
 }Skills()
 
+function displaySkill(){
+
+}
